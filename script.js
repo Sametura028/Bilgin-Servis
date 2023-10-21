bf1e0b9808897e57d6477f5f52694edb314e3cec
Sametura028 <sametucar0028@gmail.com>
2023-10-21 17:34:34 +0300

    Anasafya Tamamlandı.

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.lines').click (function(){
+    $('#line-2').toggleClass('midle');
+    $('#line-1').toggleClass('line-1');
+    $('#line-3').toggleClass('line-3');
+    $('.mobile-ver').toggleClass('open');
+    $('.scroll').toggleClass('no-scroll');
+
+  });
