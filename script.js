fbf16750fa162d7cae2fc56b17fab7004e0599dd
Sametura028 <sametucar0028@gmail.com>
2023-10-27 09:15:22 +0300

    upgrade

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-$('.lines').click (function(){
-    $('#line-2').toggleClass('midle');
-    $('#line-1').toggleClass('line-1');
-    $('#line-3').toggleClass('line-3');
-    $('.mobile-ver').toggleClass('open');
-    $('.scroll').toggleClass('no-scroll');
+$('.lines').click(function () {
+  $('#line-2').toggleClass('d-none');
+  $('#line-1').toggleClass('line-1');
+  $('#line-3').toggleClass('line-3');
+  $('.mobile-ver').toggleClass('open');
+  $('body').toggleClass('no-scroll');
+  $('html').toggleClass('no-scroll');
 
-  });
+  window.scrollTo(0, 0);
+
+});
